3deda9f101a217be0806e4c2991734decd0870cd
liutianqi17 <liutianqi17@126.com>
2021-06-29 22:46:15 +0800

    代发消息的bugfix

--- src/plugins/haruka_bot/manager.py
+++ src/plugins/haruka_bot/manager.py
@@ -9,13 +9,14 @@ groupids = []
 mode = 'text'
 manage_group_dict = eval(Bot.config.manage_group_dict)
 
-send_message_1 = on_command('send', rule=to_me(), permission=SUPERUSER, priority=5)
+send_message_1 = on_command('send', rule=to_me(), permission=SUPERUSER, priority=4)
 send_message_2 = on_message(permission=SUPERUSER, priority=5)
 
 
 # 给bot私发消息 bot会转发到指定群 指令：send test（发送到测试群）  send test voice（发送语音到测试群）
+# 群号及对应的指令在配置文件里
 @send_message_1.handle()
-async def get_args(bot: Bot, event: Event, state: T_State):
+async def get_manage_args(bot: Bot, event: Event, state: T_State):
     if event.__class__ == PrivateMessageEvent:
         args = str(event.get_message()).strip().split(' ')
         if args:
@@ -33,7 +34,7 @@ async def get_args(bot: Bot, event: Event, state: T_State):
 
 
 @send_message_2.handle()
-async def send_msg(bot: Bot, event: Event, state: T_State):
+async def send_manage_msg(bot: Bot, event: Event, state: T_State):
     if event.__class__ == PrivateMessageEvent:
         global groupids
         global mode

0053ea089af3ce2f6f34c43b803080197f4e4af8
liutianqi17 <liutianqi17@126.com>
2022-01-23 17:15:12 +0800

    通过粉丝牌子查up主

--- src/plugins/haruka_bot/dd.py
+++ src/plugins/haruka_bot/dd.py
@@ -1,6 +1,8 @@
+import re
+
 from nonebot import on_command
 from nonebot.typing import T_State
-from nonebot.adapters.cqhttp import Bot, Message, GroupMessageEvent
+from nonebot.adapters.cqhttp import Bot, Message, GroupMessageEvent, Event
 from nonebot.adapters.cqhttp.permission import GROUP_ADMIN, GROUP_OWNER
 from nonebot.permission import SUPERUSER
 from .utils import to_me, get_path, scheduler, safe_send
@@ -55,3 +57,41 @@ async def everyday_dd():
                           + vtb['sign'] + '\n粉丝数：' + str(vtb['follower']) + '\nb站空间：https://space.bilibili.com/' + \
                           str(vtb['mid']) + '/\n' + '[CQ:image,file=' + url2base64(vtb['face']) + ']'
                 await safe_send(group['bot_id'], 'group', group['groupid'], Message(message))
+
+
+# 查牌子
+medal = on_command('查牌子', priority=4)
+@medal.handle()
+async def get_bili_medal(bot: Bot, event: GroupMessageEvent, state: dict):
+    args = str(event.message).strip().split(' ')
+    if args:
+        state['name'] = args[0]
+        state['fuzzy'] = False
+        if len(args) > 1:
+            if args[1] == "模糊":
+                state['fuzzy'] = True
+        message = "[CQ:reply,id=" + str(event.message_id) + "]"
+        await safe_send(bot.self_id, 'group', event.group_id, Message("[CQ:reply,id=" + str(event.message_id) + "]查询较慢请稍等"))
+        response = requests.get("https://bili.jjnnnh.website/medal/?querylx=" + ("xzmh" if state['fuzzy'] else "") + "&query=" + state['name'])
+        html = response.content.decode("utf-8")
+        tr_content = re.findall("<th>开通人</th></tr>(.+?)</table>", html)
+        # trs = re.findall()
+        if len(tr_content) <= 0:
+            message += "没有查到捏"
+            await medal.finish(Message(message))
+        else:
+            trs = re.findall("<tr>(.+?)</tr>", tr_content[0])
+            if len(trs) <= 0:
+                message += "没有查到捏"
+                await medal.finish(Message(message))
+            else:
+                if state['fuzzy']:
+                    message += "查询到以下粉丝牌子：\n"
+                    for tr in trs:
+                        tds = re.findall("<td>(.+?)</td>", tr)
+                        message += "牌子：" + tds[1] + "，up主：" + re.findall(">(.+?)</", tds[3])[0] + "\n"
+                else:
+                    tds = re.findall("<td>(.+?)</td>", trs[0])
+                    message += "开通粉丝牌子" + re.findall(">(.+?)</", tds[1])[0] + "的up主是：" + re.findall(">(.+?)</", tds[3])[0] + "\n"
+                message += "\n数据来源：粉丝勋章库 by jjnnnh"
+                await medal.finish(Message(message))
